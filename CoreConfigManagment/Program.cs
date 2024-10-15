070e3fca8df2927e250dbf11f31ca56a65aa9f68
Rahul Ram <Rahul.Ram@us.crawco.com>
2024-10-15 06:04:06 +0530

    Adding intial files to read from json files

--- CoreConfigManagment/Program.cs
+++ CoreConfigManagment/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Create a builder object
+
+using Microsoft.Extensions.Configuration;
+
+//Create Builder Object
+var builder = new ConfigurationBuilder();
+
+// setting base path, so it knows where to find files
+builder.SetBasePath(Directory.GetCurrentDirectory());
+
+//Add multiple configuration sources
+//The ones added late in this, take precedence
+builder.AddJsonFile("mySettings.json", true,true)
+    .AddJsonFile("prod.json",true,true)
+    ;
+
+//IConfigurationRoot
+var configBuild = builder.Build(); // this build metod read all the keys and values flatens with colon and load the values in IConfigurationRoot object.
+
+//Print all first level child
+foreach (var c in configBuild.GetChildren())
+{
+    Console.WriteLine($"Key:{c.Key}, Value:{c.Value}, Path:{c.Path}");
+}
+
+Console.WriteLine("---------Children of database section");
+var sectionChild = configBuild.GetSection("Database");
+
+foreach (var c in sectionChild.GetChildren())
+    Console.WriteLine($"Key:{c.Key}, Value:{c.Value}, Path:{c.Path}");
+Console.WriteLine("--------------------------");
+
+//You can access an element by using flattend key
+Console.WriteLine($"Key : {configBuild["Database:password"]}");
